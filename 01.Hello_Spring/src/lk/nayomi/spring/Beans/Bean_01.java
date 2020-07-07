e59909e6bc71f4ab24a58c998b2465d7fa835ea4
nayomi16 <nayomirashmikaaa@gmail.com>
2020-07-07 20:46:22 +0530

    @component configaration

---
+++ 01.Hello_Spring/src/lk/nayomi/spring/Beans/Bean_01.java
@@ -0,0 +1,10 @@
+package lk.nayomi.spring.Beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bean_01 {
+    Bean_01(){
+        System.out.println("Bean Instantiated");
+    }
+}
--- 01.Hello_Spring/src/lk/nayomi/spring/main/Appconfig.java
+++ 01.Hello_Spring/src/lk/nayomi/spring/main/Appconfig.java
@@ -1,7 +1,9 @@
 package lk.nayomi.spring.main;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "lk.nayomi.spring.Beans")
 public class Appconfig {
 }
